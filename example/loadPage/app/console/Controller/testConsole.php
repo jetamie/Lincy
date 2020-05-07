a2a90673a1773bdaa4be8e8cc4a3ce606a328ee8
jetamie <webdetect@126.com>
2020-05-07 21:50:38 +0800

    update some test

---
+++ example/loadPage/app/console/Controller/testConsole.php
@@ -0,0 +1,17 @@
+<?php
+namespace example\loadPage\app\console\Controller;
+
+use library\Core\Console;
+
+class testConsole extends Console
+{
+    const STATIC_PATH = '';
+    /**
+     * 默认方法
+     * @return mixed
+     */
+    public function index()
+    {
+        // TODO: Implement index() method.
+    }
+}
---
+++ example/loadPage/app/console/Model/testModel.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace example\loadPage\app\console\Model;
+
+
+use library\Core\Model;
+
+class testModel extends Model
+{
+
+}
